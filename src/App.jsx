2d9149b0fd47f660d09996112c8a680bbf7610f4
Matias-Giorello <matiasgiorello2@gmail.com>
2026-08-06 20:57:23 -0300

    add: main features: character counter, word counter, sentence counter, and limits

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,79 @@
+import { useState } from "react"
 import { Header } from "./components/Header"
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("Esto es un texto de prueba, puedes modificarlo a tu gusto.")
+  
+  const [excludeSpaces, setExcludeSpaces] = useState(false)
+  const [limitCharacter, setLimitCharacter] = useState(false)
+  const [limitValue, setLimitValue] = useState(300)
 
-  <main>
-    <Header />
-  </main>
+  const characters = excludeSpaces ? text.replace(/\s/g, "").length : text.length
 
+  const words = text.trim() === "" ? 0 : text.trim().split("/\s+/").length
+
+  const sentences = text.trim() === "" ? 0 : text.split(/[.!?]/).filter(sentence => sentence.trim() !== "").length
+
+  const handleChangeTextArea = (e) => {
+    const value = e.target.value
+
+    if (limitCharacter) {
+      if (value.length <= limitValue) {
+        setText(value)
+      }
+    } else {
+        setText(value)
+      }
+  }
+
+  const handleChangeInputLimit = (e) => {
+    setLimitCharacter(!limitCharacter)
+    const newText = text.slice(0, limitValue)
+    setText(newText)
+  }
+
+  return (
+    <main>
+      <Header />
+      <h2>Analize your text <br />
+        in real-time</h2>
+
+      <textarea 
+      placeholder="Escribe tu texto..."
+      onChange={handleChangeTextArea}
+      value={text}
+      ></textarea>
+      <div>
+        <label>
+          <input 
+            type="checkbox" 
+            checked={excludeSpaces}
+            onChange={() => setExcludeSpaces(!excludeSpaces)}
+          />
+          Excluir espacios
+        </label>
+        <label>
+          <input 
+            type="checkbox" 
+            checked={limitCharacter}
+            onChange={handleChangeInputLimit}
+          />
+          Limite de Caracteres
+        </label>
+        {
+          limitCharacter &&
+          <input 
+          type="number" 
+          value={limitValue} 
+          onChange={(e) => setLimitValue(e.target.value)}
+          />
+        }
+      </div>
+      <p>Cantidad de Caracteres: {characters}</p>
+      <p>Cantidad de Palabras: {words}</p>
+      <p>Cantidad de Oraciones: {sentences}</p>
+    </main>
+  )
 }
 
 export  { App }
